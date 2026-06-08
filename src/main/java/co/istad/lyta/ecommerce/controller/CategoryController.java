a1e7c3b2c2b9598cb09e743e6272aff803d91fab
Phoeurnsolyta <lyta07807@gmail.com>
2026-06-08 20:25:02 +0700

    finish all endpoints

--- src/main/java/co/istad/lyta/ecommerce/controller/CategoryController.java
+++ src/main/java/co/istad/lyta/ecommerce/controller/CategoryController.java
@@ -5,6 +5,7 @@ import co.istad.lyta.ecommerce.dto.CreateCategoryRequest;
 import co.istad.lyta.ecommerce.service.CategoryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,4 +23,53 @@ public class CategoryController {
 
         return categoryService.createNew(createCategoryRequest);
     }
+
+    @ResponseStatus (HttpStatus.OK)
+    @GetMapping
+    public Page <CategoryResponse> getAllCategoriesByPagination(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "25") int size) {
+
+        return categoryService.getAllCategoriesByPagination(page, size);
+    }
+
+    @ResponseStatus (HttpStatus.OK)
+    @GetMapping("/{id}")
+    public CategoryResponse getCategoryById(
+            @PathVariable Integer id) {
+
+        return categoryService.getCategoryById(id);
+    }
+
+    @ResponseStatus (HttpStatus.OK)
+    @PutMapping ("/{id}")
+    public CategoryResponse updateCategoryById(
+            @PathVariable Integer id,
+            @Valid @RequestBody CreateCategoryRequest createCategoryRequest
+    ) {
+        return categoryService.updateCategoryById(id, createCategoryRequest);
+    }
+
+    @ResponseStatus (HttpStatus.NO_CONTENT)
+    @PatchMapping ("/{id}")
+    public void categorySoftDelete(@PathVariable Integer id) {
+        categoryService.categorySoftDelete(id);
+    }
+
+    @ResponseStatus (HttpStatus.NO_CONTENT)
+    @DeleteMapping ("/{id}")
+    public void categoryHardDelete(@PathVariable Integer id) {
+        categoryService.categoryHardDelete(id);
+    }
+
+    @ResponseStatus (HttpStatus.OK)
+    @GetMapping ("/{id}/subcategories")
+    public Page<CategoryResponse> getSubCategories(
+           @RequestParam Integer mainId,
+           @RequestParam (defaultValue = "0") int page,
+           @RequestParam (defaultValue = "25") int size
+    ) {
+        return categoryService.getSubCategories(mainId, page, size);
+    }
+
 }
--- src/main/java/co/istad/lyta/ecommerce/service/CategoryService.java
+++ src/main/java/co/istad/lyta/ecommerce/service/CategoryService.java
@@ -1,12 +1,23 @@
 package co.istad.lyta.ecommerce.service;
 
+import co.istad.lyta.ecommerce.domain.Category;
 import co.istad.lyta.ecommerce.dto.CategoryResponse;
 import co.istad.lyta.ecommerce.dto.CreateCategoryRequest;
-import org.hibernate.query.Page;
+import org.springframework.data.domain.Page;
 
 public interface CategoryService {
 
     CategoryResponse createNew (CreateCategoryRequest createCategoryRequest);
 
+    CategoryResponse getCategoryById (Integer id);
 
+    CategoryResponse updateCategoryById (Integer id, CreateCategoryRequest createCategoryRequest);
+
+    Page<CategoryResponse> getAllCategoriesByPagination(int page, int size);
+
+    void categorySoftDelete(Integer id);
+
+    void categoryHardDelete(Integer id);
+
+    Page<CategoryResponse> getSubCategories(Integer mainId, int page, int size);
 }
